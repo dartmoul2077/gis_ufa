1af4546c28a1bb40133aa76e237bf06726b08701
Aygiz <aygiz.gumerov@inbox.ru>
2025-03-17 14:38:40 +0500

    fixed header

--- src/components/Header2.vue
+++ src/components/Header2.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="h-20" style="background-color: rgba(0,0,57,255);">
-      <div class="flex justify-between px-8 py-3 h-20 m-0">
+    <div style="height: 60px; background-color: rgba(0,0,57,255);">
+      <div class="flex justify-between px-8 py-3 h-20 m-0" style="height: 60px;">
         <div class="flex items-center">
           <img src="/ноц 1.svg" alt="Logo" class="w-30">
         </div>
   
-        <ul class="flex space-x-10 items-center">
+        <ul class="h-full flex space-x-10 items-center">
           <li>
             <span class="text-white cursor-pointer hover:underline" @click="$emit('navigateToWay')">Все маршруты</span>
           </li>
@@ -30,10 +30,6 @@
           <li v-if="isLoggedIn">
             <span class="text-white cursor-pointer hover:underline"> <button @click="$emit('logout')" v-if="isLoggedIn">Выйти</button></span>
           </li>
-
-          <!-- <li >
-            <span class="text-white cursor-pointer hover:underline" @click="$emit('logout')">Выйти</span>
-          </li> -->
         </ul>
       </div>
     </div>
